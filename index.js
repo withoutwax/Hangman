b77395c194cfd87f18de35d9088a1d04ff27a50d
Will Kim <rlagmlckd@gmail.com>
2018-09-27 14:58:25 -0500

    Updated for main game mechanism

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
+let secret_word = 'WIN';
+
+let total_no_guesses = 10;
 let input_alphabet;
 let letters_guessed = {};
+let letters_guessed_output = '';
 
 function setAlphabet(alphabet) {
   input_alphabet = alphabet;
 
   if (input_alphabet in letters_guessed) {
-    document.getElementById("warning").innerHTML = "You've already clicked the value!";
+    document.getElementById("warning").innerHTML = "You've already guessed that alphabet!";
+    letters_guessed[input_alphabet] += 1
   } else {
     letters_guessed[input_alphabet] = 1
+    letters_guessed_output += input_alphabet;
   }
 
   document.getElementById("status").innerHTML = input_alphabet;
+  document.getElementById("guessed").innerHTML = letters_guessed_output;
   console.log(letters_guessed);
   // status.value = input_alphabet;
   // console.log(status.value);
 }
-
-// console.log(input_alphabet);
-// function test() {
-//   console.log(input_alphabet);
-// }
