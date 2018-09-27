0a3f258fc2fb79f17194f03220801e1bb30b8f50
Will Kim <rlagmlckd@gmail.com>
2018-09-27 13:31:56 -0500

    Updating the Hangman game with basic input system

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 let input_alphabet;
+let letters_guessed = {};
 
 function setAlphabet(alphabet) {
   input_alphabet = alphabet;
-  console.log(input_alphabet);
+
+  if (input_alphabet in letters_guessed) {
+    document.getElementById("warning").innerHTML = "You've already clicked the value!";
+  } else {
+    letters_guessed[input_alphabet] = 1
+  }
+
+  document.getElementById("status").innerHTML = input_alphabet;
+  console.log(letters_guessed);
+  // status.value = input_alphabet;
+  // console.log(status.value);
 }
 
 // console.log(input_alphabet);
