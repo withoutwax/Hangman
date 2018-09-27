a5bf4e17d9c6677824232902a62090bcd37dbb3a
Will Kim <rlagmlckd@gmail.com>
2018-09-27 13:07:31 -0500

    Updated Hangman game with input

---
+++ index.js
@@ -0,0 +1,11 @@
+let input_alphabet;
+
+function setAlphabet(alphabet) {
+  input_alphabet = alphabet;
+  console.log(input_alphabet);
+}
+
+// console.log(input_alphabet);
+// function test() {
+//   console.log(input_alphabet);
+// }
