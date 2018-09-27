ec1ee9c6f82b4dd9be283a07c194eded8a4ad55d
Will Kim <rlagmlckd@gmail.com>
2018-09-27 15:24:37 -0500

    Implemented basic game mechanic

--- index.js
+++ index.js
@@ -1,12 +1,38 @@
 let secret_word = 'WIN';
+let guess_word = [];
 
 let total_no_guesses = 10;
 let input_alphabet;
 let letters_guessed = {};
 let letters_guessed_output = '';
 
+// Update Score
+document.getElementById("scores").innerHTML = total_no_guesses;
+for (let i = 0; i < secret_word.length; i++) {
+  guess_word.push('_');
+}
+
+// DISPLAY CURRENT STATUS
+// guess_word = guess_word.join(' ');
+document.getElementById("status").innerHTML = guess_word.join(' ');
+
+
 function setAlphabet(alphabet) {
   input_alphabet = alphabet;
+
+  // CHECK IF THE ALPHABET IS IN THE SECRET WORD
+  if (secret_word.includes(input_alphabet)) {
+    for (let i = 0; i < secret_word.length; i++) {
+      if (input_alphabet == secret_word[i]) {
+        guess_word[i] = input_alphabet;
+      }
+    }
+  } else {
+    total_no_guesses -= 1;
+    document.getElementById("scores").innerHTML = total_no_guesses;
+  }
+
+  document.getElementById("status").innerHTML = guess_word.join(' '); //Update the game status
 
   if (input_alphabet in letters_guessed) {
     document.getElementById("warning").innerHTML = "You've already guessed that alphabet!";
@@ -16,7 +42,7 @@ function setAlphabet(alphabet) {
     letters_guessed_output += input_alphabet;
   }
 
-  document.getElementById("status").innerHTML = input_alphabet;
+  document.getElementById("word-input").innerHTML = input_alphabet;
   document.getElementById("guessed").innerHTML = letters_guessed_output;
   console.log(letters_guessed);
   // status.value = input_alphabet;
