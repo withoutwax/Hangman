b1b000806ef2bbe1e87cb3a3228165494ffb639c
Will Kim <rlagmlckd@gmail.com>
2018-09-28 11:06:34 -0500

    Updated the README.md

--- README.md
+++ README.md
@@ -2,3 +2,13 @@
 This is a simple hangman game where you can play.
 
 This game is originally created to teach some students to learn English vocabulary.
+
+### Things to do:
+ - [ ] Aesthetics & Design - Need to add color & make the game beautiful.
+ - [ ] Restart system - After you WIN or LOSE the game, need to restart to the original screen.
+ - [ ] Win or lose rate (in percentage). This would probably need an profile / account system.
+
+### Ideas:
+ - [ ] Input system - User might use this app as a study device (which is actually the original motivation for this game).
+ - [ ] Server / Database for the Hangman game to store vocabularies and definitions of the words.
+ - [ ] Profile / Account system - Now it's more like a flashcard app, in a Hangman game.
--- index.js
+++ index.js
@@ -6,17 +6,17 @@ let input_alphabet;
 let letters_guessed = {};
 let letters_guessed_output = '';
 
+// PRESETUP ==============================
 // Update Score
 document.getElementById("scores").innerHTML = total_no_guesses;
 for (let i = 0; i < secret_word.length; i++) {
   guess_word.push('_');
 }
-
 // DISPLAY CURRENT STATUS
 // guess_word = guess_word.join(' ');
 document.getElementById("status").innerHTML = guess_word.join(' ');
 
-
+// GAME MECHANICS =========================
 function setAlphabet(alphabet) {
   input_alphabet = alphabet;
 
@@ -31,7 +31,6 @@ function setAlphabet(alphabet) {
     total_no_guesses -= 1;
     document.getElementById("scores").innerHTML = total_no_guesses;
   }
-
   document.getElementById("status").innerHTML = guess_word.join(' '); //Update the game status
 
   if (input_alphabet in letters_guessed) {
@@ -39,12 +38,17 @@ function setAlphabet(alphabet) {
     letters_guessed[input_alphabet] += 1
   } else {
     letters_guessed[input_alphabet] = 1
-    letters_guessed_output += input_alphabet;
+    letters_guessed_output += ' ' + input_alphabet;
   }
 
   document.getElementById("word-input").innerHTML = input_alphabet;
   document.getElementById("guessed").innerHTML = letters_guessed_output;
   console.log(letters_guessed);
-  // status.value = input_alphabet;
-  // console.log(status.value);
+
+  // GAME OVER?
+  if (total_no_guesses == 0) {
+    alert("Game Over");
+  } else if (guess_word.includes('_') == false) {
+    alert("You WIN!");
+  }
 }
